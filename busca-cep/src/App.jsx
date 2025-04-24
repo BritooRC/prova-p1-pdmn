2368847c1c5c5bdb69a3a02b63e0e6e48c42846a
Rafael Brito <rafaelbc2005@gmail.com>
2025-04-24 15:12:38 -0300

    feat(p1): cria componente de busca

--- busca-cep/src/App.jsx
+++ busca-cep/src/App.jsx
@@ -1,7 +1,24 @@
-const App = () => {
-  return (
-    <h1>Hello, P1</h1>
-  )
-}
+import React from 'react'
+import Busca from './components/Busca'
+ class App extends React.Component {
+ 
+   onBuscaRealizada = (termo) => {
+     alert(termo)
+   }
+ 
+   render(){
+     return (
+       <div className='grid justify-content-center'>
+           <div className="col-12">
+             <Busca
+               onBuscaRealizada={this.onBuscaRealizada}/>
+           </div>
+           <div className="col-12">
+           </div>
+       </div>
+     )
+   }
+ }
 
 export default App
+
